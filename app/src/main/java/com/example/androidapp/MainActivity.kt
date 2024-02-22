a4b402b03bf4b7b18955055eba865cc527cd1f29
TheBackpacker1 <mohamedlouati002@gmail.com>
2024-02-22 16:18:17 +0100

    add image

--- app/src/main/java/com/example/androidapp/MainActivity.kt
+++ app/src/main/java/com/example/androidapp/MainActivity.kt
@@ -3,23 +3,33 @@ package com.example.androidapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import com.example.androidapp.ui.theme.AndroidAppTheme
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-         MessageCard(Message( "Android","Jet Compose"))
-            }
+            MessageCard(Message("Lexi", "Take a look at Jetpack Compose, it's great!"))
+
+        }
         }
     }
+
 
 
 
@@ -28,20 +38,37 @@ data class Message(val author:String,val body:String)
 
 @Composable
 fun MessageCard(msg: Message ) {
-    Text(text=msg.author)
-    Text(text=msg.body)
+    Row {
+        var modifier = Modifier.padding(all = 8.dp)
+
+        Image(
+            painter = painterResource(R.drawable.med),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                .size(120.dp)
+                .clip(CircleShape)
+        )
+
+Spacer(modifier = Modifier.width(8.dp))
+        Column {
+            
+            Text(text = msg.author)
+            Spacer(modifier = Modifier.height(4.dp))
+            Text(text = msg.body)
+        }
+    }
 }
 
-@Preview
-@Composable
 
-fun PreviewMessageCard() {
+    @Preview
+    @Composable
+    fun PreviewMessageCard() {
+        MessageCard(
+            msg = Message("Lexi", "Take a look at Jetpack Compose, it's great!")
+        )
+    }
 
-    MessageCard(
-            msg = Message("Lexi","hey,take a look at Jetpack Compose,it's great!")
-    )
 
-}
 
 
 
