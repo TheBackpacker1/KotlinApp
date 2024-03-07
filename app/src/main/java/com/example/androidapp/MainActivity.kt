97a67e8db05234b221feaa646f1a096959178237
mohamed <mohamedlouati002@gmail.com>
2024-03-07 09:30:29 +0100

    last changes

--- app/src/main/java/com/example/androidapp/MainActivity.kt
+++ app/src/main/java/com/example/androidapp/MainActivity.kt
@@ -5,9 +5,12 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
+import androidx.compose.animation.animateContentSize
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.border
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
@@ -30,6 +33,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -54,7 +58,7 @@ class MainActivity : ComponentActivity() {
     @Composable
     fun MessageCard(msg: Message) {
         Row {
-            var modifier = Modifier.padding(all = 8.dp)
+            var modifier = Modifier.padding(all = 12.dp)
 
             Image(
                 painter = painterResource(R.drawable.med),
@@ -68,6 +72,9 @@ class MainActivity : ComponentActivity() {
             Spacer(modifier = Modifier.width(8.dp))
 
             var isExpanded by remember { mutableStateOf(false) }
+            val surfaceColor by animateColorAsState(
+                if(isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface,
+            )
             Column(modifier =Modifier.clickable { isExpanded =!isExpanded}){
 
                 Text(
@@ -76,7 +83,14 @@ class MainActivity : ComponentActivity() {
                     style = MaterialTheme.typography.titleSmall
                 )
                 Spacer(modifier = Modifier.height(4.dp))
-                Surface(shape = MaterialTheme.shapes.medium, shadowElevation = 5.dp) {
+                Surface(shape = MaterialTheme.shapes.medium,
+                    shadowElevation = 5.dp,
+                    color = surfaceColor ,
+                    modifier = Modifier.animateContentSize().padding(1.dp)
+
+
+
+                ) {
 
                     Text(
                         text = msg.body,
@@ -96,7 +110,8 @@ class MainActivity : ComponentActivity() {
 
     @Composable
     fun Conversation(messages: List<Message>) {
-        LazyColumn {
+        LazyColumn(    verticalArrangement = Arrangement.spacedBy(16.dp)
+        ) {
 
             items(messages) { message ->
                 MessageCard(message)
